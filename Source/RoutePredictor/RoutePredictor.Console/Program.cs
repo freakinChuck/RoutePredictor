c0fcd66739f472de53bcfc114dfa19369edd651d
Silvio <freakinChuck@users.noreply.github.com>
2026-02-16 11:54:06 +0100

    basic random data and console api

--- Source/RoutePredictor/RoutePredictor.Console/Program.cs
+++ Source/RoutePredictor/RoutePredictor.Console/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using RoutePredictor.Core;
+
 Console.WriteLine("Hello, World!");
+
+
+Console.Write("Enter StartKM: ");
+var startKm = float.Parse(Console.ReadLine()!);
+Console.Write("Enter EndKM: ");
+var endKM = float.Parse(Console.ReadLine()!);
+
+Console.Write("Enter AbflussM3: ");
+var abflussM3 = float.Parse(Console.ReadLine()!);
+
+Console.WriteLine();
+Console.WriteLine();
+
+var predictor = new FlowPredictor();
+var prediction = predictor.PredictTime(startKm, endKM, abflussM3);
+
+Console.WriteLine($"Prediction Time: {prediction.ToString("n2")} min");
+
+
+
---
+++ Source/RoutePredictor/RoutePredictor.Core/FlowModels.cs
@@ -0,0 +1,17 @@
+﻿namespace RoutePredictor.Core
+{
+    using Microsoft.ML.Data;
+    public class FlowSample
+    {
+        [LoadColumn(0)] public float StartKm { get; set; }
+        [LoadColumn(1)] public float EndKm { get; set; }
+        [LoadColumn(2)] public float AbflussM3 { get; set; }
+        [LoadColumn(3)] public float TimeMin { get; set; } // Label
+    } 
+    public class FlowPrediction 
+    { 
+        [ColumnName("Score")] public float TimeMin { get; set; } 
+    }
+
+    public class DistanceOutput { public float Distance { get; set; } }
+}
